344ccad48cf659de8e8be7d50f50205536dc279b
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-06-21 18:47:54 +0200

    minor update

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -11,6 +11,7 @@ const Projects = () => (
     <div className="projects__title">Projets</div>
     <div className="projects__card_box">
       {
+        // eslint-disable-next-line no-unused-vars
         projectsData.map((item, i) => (
 
           <div className="projects__card" key={item.id}>
@@ -18,7 +19,7 @@ const Projects = () => (
               <img src={OboardScreen} alt="screen site oboardgame" />
             </div>
             <div className="projects__card__content">
-              <div className="projects__card__content__name">{item.name}</div>
+              <div className="projects__card__content__name"><a href="https://github.com/KillianCourvoisier/oboardgame-project-Nautilus">{item.name}</a></div>
               <div className="projects__card__content__excerpt">
                 <p>{item.excerpt}
                 </p>
--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export default [
     name: 'O\'boardgames',
     excerpt: [
       'Oboardgames est un projet de fin d\'étude réalisé en Mars 2021 avec 4 autres camarades de promo chez O\'clock. ',
-      'Le site n\'est plus disponible en ligne, mais vous pouvez le retrouvez ici, sur mon Github!',
+      'Le site n\'est plus disponible en ligne, mais vous pouvez le retrouvez sur mon Github!',
     ],
   },
 ];
